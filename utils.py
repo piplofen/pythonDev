02190ceab2d5ff2dc4f15fc5cbd2415c6b0034de
piplofen <podkjovyrov19@gmail.com>
2023-03-21 15:04:37 +0200

    delete test file and add utils file

---
+++ utils.py
@@ -0,0 +1,45 @@
+import csv
+import json
+import logging as log
+import os
+
+# headersForQue = os.environ.get("HEADERSFORQUEUE")
+
+
+# открываю файлик
+def openFile(path: str, type: str = None):
+    try:
+        with open(f"{path}.{type}", encoding="utf-8") as file:  # !!!!!!!!!!!!!!!!! encoding="utf-8" !!!!!!!!!!!!!!!!!
+            if type == "json":
+                data = json.load(file)
+            elif type == "csv":
+                data = csv.reader(file, delimiter=";")
+            elif type is None:
+                return file
+
+        return data
+    except Exception as e:
+        print(e)
+        return False
+
+
+# записываю в файлик
+def writeFile(path: str, type: str, param: str, data: object, print1=0):
+    try:
+        with open(f"{path}.{type}", param) as file:
+            if type == "json":
+                data = json.dump(data, file, indent=1)
+            elif type == "csv":
+                data = csv.writer(file)  # ?
+        if print1 == 1:
+            print(f"Файл {path}.{type} записан успешно")
+    except FileNotFoundError as e:
+        print("Файл не найден")
+
+
+def logStart(filePath):
+    return log.basicConfig(level="INFO", encoding="utf-8", filename=filePath,
+                           filemode="a",
+                           format="[%(asctime)s] - [%(levelname)s] - [%(pathname)s.%(filename)s.%(funcName)s(%("
+                                  "lineno)d)] > [%(message)s]")
+
