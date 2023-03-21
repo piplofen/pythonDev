35d3ca8ed8a4c21bbfc575be49eded017d9476ea
piplofen <podkjovyrov19@gmail.com>
2023-03-21 16:00:30 +0200

    nothing of note

--- workplaceForDirector/main.py
+++ workplaceForDirector/main.py
@@ -1,6 +1,25 @@
 import utils as u
 import logging as log
+import sys
+
+from PyQt6 import QtWidgets
+from design import logInDesign
 
 u.logStart("log/log.log")
 
-log.info("test logger")
+
+class MainWindow(QtWidgets.QMainWindow, logInDesign.Ui_LogUpWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
+
+
+if __name__ == '__main__':
+    main()
